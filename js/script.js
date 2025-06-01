ee1acef06a4aa55582e108462b179c296783bd54
Takashi Ota <gmkzkks1011@gmail.com>
2025-06-01 21:30:34 +0900

    js-loading--screenにアニメーションを追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* loadingから画面遷移 */
 const loadingAreaGrey = document.querySelector('.js-loading');
+const loadingAreaGreen = document.querySelector('.js-loading--screen');
 
 window.addEventListener('load', () => {
   // ローディング中（グレースクリーン）
@@ -12,6 +13,19 @@ window.addEventListener('load', () => {
       duration: 2000,
       delay: 1200,
       easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（グリーンスクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      eading: 'eaase',
       fill: 'forwards',
     }
   );
