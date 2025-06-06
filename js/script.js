5eb27a1dcfbfa7e3ce25bfd3badae62f8e979b90
Takashi Ota <gmkzkks1011@gmail.com>
2025-06-06 17:23:34 +0900

    unobserveで一度実行した処理は再び実行されないようにする

--- js/script.js
+++ js/script.js
@@ -106,10 +106,23 @@ menuClose.addEventListener('click', () => {
 
 /* fadein */
 // 監視対象が範囲内に現れたら実行する動作
-const animateFade = (entries) => {
+const animateFade = (entries, obs) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-    // console.log(entry.target);
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'],
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+      // 一度ふわっと表示されたら監視を辞める
+      obs.unobserve(entry. target);
     }
   });
 };
